6a98f7ba07c7755c8ced64fe35403acb5b71115e
mithunmahato <mahatomithun3@gmail.com>
2024-09-22 17:27:26 +0530

    folder modified

--- netmikotest/netmikoclassmapper.py
+++
@@ -1,4 +0,0 @@
-from netmiko.ssh_dispatcher import CLASS_MAPPER_BASE
-from rich import print
-
-print(list(CLASS_MAPPER_BASE.keys()))
--- test.py
+++ test.py
@@ -2,3 +2,20 @@ import os
 
 x=os.getsource(os)
 print(x)
+
+print("Programming","Essentials","in", sep = "***", end = "...")
+print("Python")
+
+print("Tell me anything...")
+anything = input()
+print("Hmm...", anything, "... Really?")
+
+anything = float(input("Enter a number: "))
+something = anything ** 2.0
+print(anything, "to the power of 2 is", something)
+
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
+
